75d85ebe2f2d4513e09288372d52b98dfde912d1
gabrielorsatti <gabriel.orsatti@gmail.com>
2026-04-22 17:31:56 +0200

    chore: setup vite + tailwind v4 foundation

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return (
+    <div className="bg-brand min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Gym Track Landing est prête !</h1>
+    </div>
+  )
+}
+
+export default App
